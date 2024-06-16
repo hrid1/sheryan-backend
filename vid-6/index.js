5b337d89baa928bcbeaae4cac619964d731d0f12
hrid1 <hridoycse1@gmail.com>
2024-06-17 01:42:07 +0600

    video-7

--- vid-6/index.js
+++ vid-6/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const path = require('path');
 
 // middleware for body parer
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(path))
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     // res.send("Hellow world vai")
     res.render("index");
+})
+
+app.get('/profile/:username', (req, res) => {
+    res.send(`Welcome ${req.params.username} !`)
+})
+
+app.get('/profile/:username/:age', (req, res) => {
+    res.send(`Your name is ${req.params.username} and Age is: ${req.params.age} .`)
 })
 
 app.listen(port, () => {
